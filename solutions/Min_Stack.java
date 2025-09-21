44e411d5ef2162bb84cafaa662ccf52c9a20fc6f
StaceyO21 <staceyobrien21@gmail.com>
2025-09-20 19:50:30 -0500

    min stack

--- solutions/Min_Stack.java
+++ solutions/Min_Stack.java
@@ -1,23 +1,37 @@
+import java.util.Stack;
+
 class MinStack {
 
-    public MinStack() {
+    private Stack<Integer> stack = new Stack<>();
 
+    public MinStack() {
+         stack = new Stack<>;
     }
 
     public void push(int val) {
-
+        stack.push(val);
     }
 
     public void pop() {
-
+        stack.pop();
     }
 
     public int top() {
-
+        return stack.peek();
     }
 
     public int getMin() {
+        Stack<Integer> tmp = new Stack<>;
+        int mini = stack.peek();
 
+        while(!stack.isEmpty()){
+            mini = Math.min(mini, stack.peek());
+            tmp.push(stack.pop());
+        }
+        while(!tmp.isEmpty()){
+            stack.push(tmp.pop());
+        }
+        return mini;
     }
 }
 
