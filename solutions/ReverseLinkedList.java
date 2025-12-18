6fe859195ed7ba54dbf4a07dc1015b344a978f0d
StaceyO21 <staceyobrien21@gmail.com>
2025-12-18 16:32:22 -0600

    Iteration of #29

--- solutions/ReverseLinkedList.java
+++ solutions/ReverseLinkedList.java
@@ -1,17 +1,28 @@
 
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
-        if(head == null){
-            return null;
-        }
+//        if(head == null){
+//            return null;
+//        }
+//
+//        ListNode newHead = head;
+//        if(head.next != null) {
+//            newHead = reverseList(head.next);
+//                head.next.next = head;
+//        }
+//        head.next = null;
+//
+//        return newHead;
+//    }
+//}
+        ListNode prev = null;
+        ListNode curr = head;
 
-        ListNode newHead = head;
-        if(head.next != null) {
-            newHead = reverseList(head.next);
-                head.next.next = head;
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
         }
-        head.next = null;
-
-        return newHead;
+        return prev;
     }
-}
